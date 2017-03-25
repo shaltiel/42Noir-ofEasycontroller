708da0cf34dcb71a40294eaa084e6e046f21ab39
shaltiel eloul <shaltiele@gmail.com>
2017-03-25 15:42:44 +0000

    add comments

--- escon.h
+++ escon.h
@@ -33,17 +33,18 @@ public:
     char * rec();//receive raw string message
     int rec(int); //give value in index of the controller, see table function.
     float rec(int,float,float);//return remapped value from index
-    float rec(string,float, float);//return remapped value from  string index
+    float rec(string,float, float);//return with a new range from string index
     int rec(string);
     void snd(string,int);
     void snd_wait(int,int);
     void snd(int,int);
     void label(string,string); //send label to controller
-    void set_to_preset(string,int);
-    void call_preset(int);
+    void set_to_preset(string,int);// set a knob  with string name to control a preset number (in update function only)
+    void call_preset(int); // call a preset (use in setup function)
     int table(string); //int index to string index
     int hash(int);
     int re_hash(int);
-    void all_s(int); void all_p(int);
+    void all_s(int);//set all slides to value use in setup function.
+    void all_p(int);//set all pads to value use in setup function.
 };
 
